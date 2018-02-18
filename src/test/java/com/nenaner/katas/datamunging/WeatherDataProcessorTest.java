deb2aa92d1bc4f5c367da864572e0faa6367dd3c
AddictedToBattlestar <andy@Ryan-MacBook-Pro.local>
2018-02-17 22:13:55 -0500

    variation with an alternate test

--- src/test/java/com/nenaner/katas/datamunging/WeatherDataProcessorTest.java
+++ src/test/java/com/nenaner/katas/datamunging/WeatherDataProcessorTest.java
@@ -1,5 +1,6 @@
 package com.nenaner.katas.datamunging;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -10,7 +11,6 @@ import org.slf4j.Logger;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
@@ -23,16 +23,17 @@ public class WeatherDataProcessorTest {
     @InjectMocks
     private WeatherDataProcessor subject;
 
-    private Stream<String> fakeStream;
-    private String[] fakeInputStreamData = { "   1  10    19","   2  20    28", "   3  30    41"};
     private Logger mockLogger;
 
     @Before
     public void setup() throws IOException, URISyntaxException {
         MockitoAnnotations.initMocks(this);
 
-        fakeStream = Arrays.stream(fakeInputStreamData);
-        when(mockResourceFileHelper.getResourceFileAsInputStream(anyString())).thenReturn(fakeStream);
+        String[] fakeInputStreamData = {
+                generateFakeInputData(1, 10, 20),
+                generateFakeInputData(2, 20, 28),
+                generateFakeInputData(3, 30, 41)};
+        when(mockResourceFileHelper.getResourceFileAsInputStream(anyString())).thenReturn(Arrays.stream(fakeInputStreamData));
 
         mockLogger = mock(Logger.class);
         subject.logger = mockLogger;
@@ -48,5 +49,25 @@ public class WeatherDataProcessorTest {
     public void basicLoggingTest() throws IOException, URISyntaxException {
         subject.outputDayWithSmallestTemperatureSpread();
         verify(mockLogger).info("2");
+    }
+
+    @Test
+    public void basicLoggingTestVariation() throws IOException, URISyntaxException {
+        String[] fakeInputStreamData = {
+                generateFakeInputData(1, 10, 20),
+                generateFakeInputData(2, 20, 28),
+                generateFakeInputData(3, 28, 41),
+                generateFakeInputData(4, 41, 42)
+        };
+        when(mockResourceFileHelper.getResourceFileAsInputStream(anyString())).thenReturn(Arrays.stream(fakeInputStreamData));
+
+        subject.outputDayWithSmallestTemperatureSpread();
+        verify(mockLogger).info("4");
+    }
+
+    private String generateFakeInputData(Integer dayOfMonth, Integer lowTemp, Integer highTemp) {
+        return StringUtils.leftPad(dayOfMonth.toString(), 4) +
+                StringUtils.leftPad(lowTemp.toString(), 4) +
+                StringUtils.leftPad(highTemp.toString(), 6);
     }
 }
